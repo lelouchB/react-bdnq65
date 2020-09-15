cc4983cd985b22c64bcf91f51254c50c4cbde50d
lelouchB <ashutoshs.ic18@nsut.ac.in>
2020-09-15 13:25:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bdnq65
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bdnq65)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ width: "400px", height: "400px", background: "blue" }}></div>
+  );
+}
