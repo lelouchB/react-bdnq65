34e1d3996488d1f467b1495368fd913830cdb5b2
lelouchB <ashutoshs.ic18@nsut.ac.in>
2020-09-15 14:59:20 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 
 export default function App() {
   return (
-    <div onMouseEnter={()=>{console.log("Event:MouseEnter")}}  onMouseLeave={()=>{console.log("Event:MouseLeave")}} style={{ width: "400px", height: "400px", background: "blue" }}></div>
+    <div
+      onMouseEnter={() => {
+        console.log("Event:MouseEnter");
+      }}
+      onMouseLeave={() => {
+        console.log("Event:MouseLeave");
+      }}
+      style={{ width: "400px", height: "400px", background: "blue" }}
+    />
   );
 }
